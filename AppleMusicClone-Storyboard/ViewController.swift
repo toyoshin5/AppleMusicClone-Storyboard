8ec05e383302ada6b305ff6995e6627e232b9994
Shingo TOYODA <toyodasinngo@gmail.com>
2023-11-04 22:19:29 +0900

    Initial Commit

---
+++ AppleMusicClone-Storyboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppleMusicClone-Storyboard
+//
+//  Created by Shin Toyo on 2023/11/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
